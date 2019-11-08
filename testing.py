1f61fd361a4536cf17b7698fd8d4260f5140d475
kamilprz <kamil.prz@gmail.com>
2019-11-08 16:53:36 +0000

    repo name + most recent commit

--- testing.py
+++ testing.py
@@ -5,6 +5,5 @@ from github import Github
 g = Github("kamilprz", "password")
 
 for repo in g.get_user().get_repos():
-    print(repo.name)
-
-
+    branch = repo.get_branch("master")
+    print("{0:70} Most Recent {1:80}".format(str(repo), str(branch.commit)))
